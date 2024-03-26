ac66aa54fd68671b60ce8087c970f0029bd9312e
sky <sky-devops@liongaming.co>
2024-03-26 10:32:50 +0800

    temp

--- pkg/util/yuan_cent.go
+++ pkg/util/yuan_cent.go
@@ -3,23 +3,31 @@ package util
 import "fmt"
 
 // YuanToCent 元转分
-func YuanToCent(yuan float64) int64 {
+func YuanToCent(yuan float64) (int64, error) {
 	dec, err := NewFromString(fmt.Sprintf("%0.2f", yuan))
-	CheckErr(err)
+	if err != nil {
+		return 0, err
+	}
 	m, err := NewFromString("100")
-	CheckErr(err)
-
-	return dec.Mul(m).IntPart()
+	if err != nil {
+		return 0, err
+	}
+	return dec.Mul(m).IntPart(), nil
 }
 
 // CentToYuan 分转元
-func CentToYuan(cent int64) float64 {
+func CentToYuan(cent int64) (float64, error) {
 	centDec, err := NewFromString(fmt.Sprintf("%d", cent))
-	CheckErr(err)
+	if err != nil {
+		return 0, err
+	}
 	mDec, err := NewFromString(fmt.Sprintf("%d", 100))
-	CheckErr(err)
-
-	result, _ := centDec.Div(mDec).Round(2).Float64()
-	CheckErr(err)
-	return result
+	if err != nil {
+		return 0, err
+	}
+	result, ok := centDec.Div(mDec).Round(2).Float64()
+	if ok {
+		return 0, err
+	}
+	return result, nil
 }
